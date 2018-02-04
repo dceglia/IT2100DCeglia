75bf2e4d80d296048dbd7c2e8e1386a85e425bbb
dceglia <s01139887@acad.tri-c.edu>
2018-02-04 17:43:23 -0500

    Lab 3 update push

---
+++ Lab03DCeglia.playground/Contents.swift
@@ -0,0 +1,50 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+// *********************** //
+//          TWO            //
+// *********************** //
+
+22/7     // = 3.....Integer division results in the nearest whole integer
+
+22.0/7.0     // = 3.142857142857143.....Double division results
+Double(22.0/7.0)     // = same ^^ .....in a more precise answer (double precision)
+
+Float(22.0/7.0)     // = 3.142857.....Float division is made up of 8 bits
+
+
+// *********************** //
+//         THREE           //
+// *********************** //
+
+class Counter {
+    var count = 0
+
+    func increment() {
+        count += 1
+    }
+    
+    func decrement() {
+        count -= 1
+    }
+    
+    //func decrement() {
+        
+    //}
+    
+    func reset() {
+        count = 0
+    }
+
+}
+
+func combineAll(numbers: [Int], starter: String) -> String {
+    var numbersCombined = starter + ": "
+    for number in numbers {
+        numbersCombined = numbersCombined + String(number) + " "
+    }
+    return numbersCombined
+}
+print(combineAll(numbers: [9, 8, 7], starter: "Combined"))
