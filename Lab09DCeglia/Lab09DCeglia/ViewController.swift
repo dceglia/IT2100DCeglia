44d19defef7e1b6cacdaa93638ebb436130cc093
dceglia <s01139887@acad.tri-c.edu>
2018-03-20 23:45:36 -0400

    Lab09 update 1

--- Lab09DCeglia/Lab09DCeglia/ViewController.swift
+++ Lab09DCeglia/Lab09DCeglia/ViewController.swift
@@ -9,7 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var userOutput: UILabel!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var userEmail: UILabel!
+    @IBOutlet weak var favoriteLanguage: UILabel!
+    @IBOutlet weak var selectedAction: UITextView!
+    
+    @IBAction func showAlertWithFields(_ sender: Any) {
+        let alertController = UIAlertController(title: "", message: "", preferredStyle: UIAlertControllerStyle.alert)
+        
+        let defaultAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil)
+        
+        alertController.addAction(defaultAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
