29de3a896222c183d90a1d781c43b9a85aaa0bce
dceglia <s01139887@acad.tri-c.edu>
2018-03-31 20:33:28 -0400

    Lab 10 steps 1-4

---
+++ Lab10-MultipleScenes/Lab10-MultipleScenes/Scene2ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  Scene2ViewController.swift
+//  Lab10-MultipleScenes
+//
+//  Created by David Ceglia on 3/31/18.
+//  Copyright © 2018 David Ceglia. All rights reserved.
+//
+
+import UIKit
+
+class Scene2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
