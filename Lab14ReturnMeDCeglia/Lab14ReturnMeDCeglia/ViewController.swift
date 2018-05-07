9c1b6edeacd9b045c9eebc056d3ee56d2ad48280
DNCeg <s01139887@acad.tri-c.edu>
2018-05-07 18:01:03 -0400

    Lab 14 - program II upload - Return Me

---
+++ Lab14ReturnMeDCeglia/Lab14ReturnMeDCeglia/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Lab14ReturnMeDCeglia
+//
+//  Created by David Ceglia on 5/7/18.
+//  Copyright © 2018 David Ceglia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let kName:String = "name"
+    let kEmail:String = "email"
+    let kPhone:String = "phone"
+    let kPicture:String = "picture"
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
